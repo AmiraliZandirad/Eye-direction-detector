611cd18f30985940bac2bc79b21952686a90fba9
AmiraliZandirad <AmiraliZandirad@outlook.com>
2024-11-24 17:55:49 +0330

    left eyes Coordinates

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ from cvzone.FaceMeshModule import FaceMeshDetector
 
 detector = FaceDetector()
 meshdetector = FaceMeshDetector(maxFaces=1)
+
+left_eye = [362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398]
+
+
+cv.destroyAllWindows()
